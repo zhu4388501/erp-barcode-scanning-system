a326afc9332a0b8db4f6343ccb3bd5afaf0fa58c
zhu4388501 <sj15778554834@163.com>
2025-12-05 15:25:59 +0800

    Add schemas/__init__.py file

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+from .product import ProductBase, ProductCreate, ProductResponse
+from .scan_record import ScanRecordBase, ScanRecordCreate, ScanRecordResponse
+from .stock import StockQueryResponse
+from .offline_operation import OfflineOperationCreate
+from .backup import BackupCreate
